8e6042fee8f37d04519a47b0db00cdb791bda829
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-24 09:00:14 +0300

    Mission Passed

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -7,11 +7,11 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
-  useEffect (() => {
+  useEffect(() => {
     fetch("http://localhost:4000/items")
-    .then((r) => r.json())
-    .then((items) => console.log(items))
-  })
+      .then((r) => r.json())
+      .then((items) => setItems(items));
+  }, []);
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
